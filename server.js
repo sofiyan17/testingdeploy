1730333aff8e4c4481243058a271d614a856efbc
sofiyan17 <muhammadsofian535@gmail.com>
2024-06-16 02:03:01 +0800

    revisi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-const destinations = require("../data/destinations");
+const destinations = require("./data/destinations");
 
 // Mengaktifkan CORS untuk semua rute
 app.use(cors());
